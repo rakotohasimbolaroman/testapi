fe6f7771b55e7c875183531339eb4e7afb223464
Rakotohasimbola Roman <Luoman424@gmail.com>
2026-09-24 14:01:33 +0200

    Add '/aide' endpoint for sqlite3 help
    Added a new endpoint '/aide' to provide help with sqlite3.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from sqlite3 import *
 app = FastAPI()
 
 @app.get("/")
@@ -6,6 +7,10 @@ app = FastAPI()
 def accueil():
 	return {"message":"API Growatt"}
 
+@app.get("/aide")
+def aide():
+	return {help(sqlite3)}
+	
 @app.post("/devis")
 def creer_devis(client,num):
 	return{
