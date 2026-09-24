01a017850068325f87cc0db43133634a70d87241
Rakotohasimbola Roman <Luoman424@gmail.com>
2026-09-24 13:07:34 +0200

    Ajout devis
    Added a new POST endpoint '/devis' to create a quote.

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 
 def accueil():
 	return {"message":"API Growatt"}
+
+@app.post("/devis")
+def creer_devis(client,num):
+	return{
+		"success":True,
+		"Client": client,
+		"num": num
+	}
