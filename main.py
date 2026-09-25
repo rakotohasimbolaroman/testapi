152b168035e71e8c57849e20f6de7936a834a031
Rakotohasimbola Roman <Luoman424@gmail.com>
2026-09-25 06:22:47 +0200

    Refactor FastAPI routes and database handling
    Refactored FastAPI routes and ensured database connections are properly closed.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 import sqlite3
 app = FastAPI()
-con = sqlite3.connect("data.db")
-cur = con.cursor()
-@app.get("/")
 
+@app.get("/")
 def accueil():
 	return {"message":"Bienvenue Chez Growatt Madagascar"}
 
 @app.get("/allunite")
 def getallunite():
+	con = sqlite3.connect("data.db")
+	cur = con.cursor()
 	tout = cur.execute("SELECT * FROM unite")
 	f = {}
 	for i in tout:
@@ -24,6 +24,7 @@ def getallunite():
 	f = {}
 	for i in tout:
 		f[i[0]] = i[1]
+	con.close()
 	return f
 	
 @app.post("/devis")
